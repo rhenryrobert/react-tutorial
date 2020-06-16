de60a7532ab7038f97b9caa8084560500d652e2c
Henry Robert <henry.robert@position2.com>
2020-06-16 07:31:05 +0530

    Updates

---
+++ precache-manifest.e2e77fb471a2eed5784a7f4015b0d896.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ba14c82fecb4cb94065f05942e30827",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "17051e8a1de5bc8abc95",
+    "url": "/react-tutorial/static/css/main.9447384d.chunk.css"
+  },
+  {
+    "revision": "709fc1b6e60c375734c7",
+    "url": "/react-tutorial/static/js/2.4106f50a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.4106f50a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17051e8a1de5bc8abc95",
+    "url": "/react-tutorial/static/js/main.8bb2ed89.chunk.js"
+  },
+  {
+    "revision": "b6f2badd4a096922496c",
+    "url": "/react-tutorial/static/js/runtime-main.ee9240c0.js"
+  }
+]);
